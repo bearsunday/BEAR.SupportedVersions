e2dccfac695a086232ea9a40b931259d57e09eee
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-31 14:31:35 +0900

    add function ucwords

---
+++ polyfill/ucwords.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Resource;
+
+/**
+ * ucwords for obsolete php version 5.5.15 or down
+ *
+ * @param $str
+ * @param string $delimiter
+ *
+ * @return string
+ */
+function ucwords($str, $delimiter = ' ') {
+    return str_replace(' ', '', \ucwords(str_replace($delimiter, ' ', $str)));
+}
